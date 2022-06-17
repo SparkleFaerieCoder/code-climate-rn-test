024188b81d8e50005dfbed79488765f7f889c40c
Alex Silcox <alex@echobind.com>
2022-06-17 16:34:49 -0700

    remove test

--- screens/_tests__/NotFoundScreen.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import NotFoundScreen from '../NotFoundScreen';
-
-test('renders correctly', () => {
-  const tree = renderer.create(<NotFoundScreen/>).toJSON();
-  expect(tree).toMatchSnapshot();
-});
